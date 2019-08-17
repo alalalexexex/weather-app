8e0af391ef35e3b93a349535e0dd40ec222738d9
Alex <alextrainham@gmail.com>
2019-08-17 12:14:06 -0400

    updates

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 const request = require('request');  
 const { geocode } = require('./utils/geocode'); 
+const { forecast } = require('./utils/forecast')
 
 geocode(process.argv[2], (err, data) => {
     if(err) console.log(err); 
-    else{
-        const url = "https://api.darksky.net/forecast/94c147c6b8e27f69af6273a051af3156/" + data.longitude +","+ data.latitude;
-        request({url: url, json: true}, (error, response, body) => {
-            if(error){
-                console.log(error); 
-            }else if (response.body.error){
-                console.log(response.body.error); 
-                console.log('Darksky is returning a bad value'); 
-            }else{
-                console.log('Current weather for...', data.location); 
-                console.log(body.daily.data[0].summary); 
-                console.log('It is currently' , body.currently.temperature, 'degrees out.', body.currently.precipProbability, '% chance of rain.')
-            }
+    else {
+        console.log(data.location); 
+        forecast(data.latitude, data.longitude, (error, data) => {
+            console.log(error); 
+            console.log(data); 
         })
     }
 }); 
